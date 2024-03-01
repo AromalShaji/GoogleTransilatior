9acb5ff5ea7d255d10c48274835c2ebb422c9a69
AromalShaji <aromalpshaji10@gmail.com>
2024-03-01 23:43:15 +0530

    11

--- Translator/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import *
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('translate_text', translate_text, name='translate_text'),
-]

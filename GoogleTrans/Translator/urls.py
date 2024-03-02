fe12caed3ba24e9289331fa002035f0d3f39ad50
AromalShaji <aromalpshaji10@gmail.com>
2024-03-02 12:58:09 +0530

    23

--- GoogleTrans/Translator/urls.py
+++ GoogleTrans/Translator/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', login, name='login'),
+    path('home', home, name='home'),
     path('translate_text', translate_text, name='translate_text'),
-    path('login', login, name='login'),
     path('register', register, name='register'),
     path('logout', logout, name='logout'),
     path('history', history, name='history'),
